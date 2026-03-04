37789a1579bfc3750e57baffa443c38c7450dfb2
Sakume45 <138834177+Sakume45@users.noreply.github.com>
2026-03-04 15:48:27 -0300

    Add files via upload

---
+++ code (6).py
@@ -0,0 +1,4 @@
+listaInicial = ["Biscoito", "Bolacha", "Bolo", "Arroz", "Feijão"]
+ItemAd = input("Digite um item para adicionar: ")
+listaInicial.append(ItemAd)
+print(f"Lista atualiazada: ", {listaInicial})
