e4aa3f661d5a63a38fb9612c8d07b3f210553c15
Sakume45 <138834177+Sakume45@users.noreply.github.com>
2026-03-12 08:20:34 -0300

    Add files via upload

---
+++ code (23).py
@@ -0,0 +1,9 @@
+# Gere uma função que mostre a soma e o produto de dois números
+
+def mathematiks(a, b):
+    soma = a + b
+    prod = a * b
+    return soma, prod
+    
+so, po = mathematiks(10, 12)
+print(f"Soma: {so} e Produto: {po}")
